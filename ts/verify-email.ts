16883f9be4bf06345d3268f6ebdad23907da367c
YoungCoderVN <bravo03vn@gmail.com>
2026-08-16 08:44:51 +0700

    Delete ts directory

--- ts/verify-email.ts
+++
@@ -1,6 +0,0 @@
-const emailElement = document.querySelector<HTMLParagraphElement>("#verificationEmail");
-const email = new URLSearchParams(window.location.search).get("email");
-
-if (emailElement) {
-  emailElement.textContent = email || "your email address";
-}
