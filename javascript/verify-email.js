eb951740fcfdce04ad42800dfd23a232aa238875
YoungCoderVN <bravo03vn@gmail.com>
2026-08-16 08:44:19 +0700

    Delete javascript directory

--- javascript/verify-email.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const emailElement = document.querySelector("#verificationEmail");
-const email = new URLSearchParams(window.location.search).get("email");
-if (emailElement) {
-    emailElement.textContent = email || "your email address";
-}
